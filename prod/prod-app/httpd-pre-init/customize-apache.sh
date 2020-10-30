d392ebb70f14ad8cbeeaf099ba93ed60a3d6c9ac
jaimelm <jaimelm@umich.edu>
2020-10-30 07:22:37 -0400

    fixing check

--- prod/prod-app/httpd-pre-init/customize-apache.sh
+++ prod/prod-app/httpd-pre-init/customize-apache.sh
@@ -5,6 +5,6 @@
 sed -i 's#/opt/rh/httpd24/root/var/www/cgi-bin#/opt/app-root/src/cgi-bin#g' /etc/httpd/conf/httpd.conf
 
 # Set an httpd environment variable based on the container environment variable. This is the location where the data files with be written. 
-if [[ -v ${DATA_DIR} ]]; then
+if [[ -v DATA_DIR ]]; then
 	echo "SetEnv DATA_DIR ${DATA_DIR}" >> /etc/httpd/conf/httpd.conf
 fi
